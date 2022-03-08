d4c537da5733cf023b950116453e0d269b223e55
Dustin Grof <dustingrof@MacBook-Pro.local>
2022-03-07 18:37:44 -0800

    update tail.js to include check for edge case

--- tail.js
+++ tail.js
@@ -4,12 +4,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-  return array.slice(1, array.length);
+  return array.length > 1 ? array.slice(1, array.length) : [];
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// Test Case 1: Check the returned array elements
+const words = ["Hello", "Lighthouse", "Labs"];
+const result = tail(words);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-//assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+// Test Case: Check the original array
+assertEqual(words.length, 3); // original array should still have 3 elements!
