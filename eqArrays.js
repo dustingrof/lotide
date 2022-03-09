034e6fa552fefaa2c9c4a1eda5fd711690c27831
Dustin Grof <dustingrof@MacBook-Pro.local>
2022-03-08 20:27:09 -0800

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  return (actual === expected) ? console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+//test cases below
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), false); // => should FAIL
