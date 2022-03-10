1fa9b470f2d173c39ed507627a1bbf7ddc1c1cf5
Dustin Grof <dustingrof@MacBook-Pro.local>
2022-03-10 00:51:33 -0800

    add the findKeyByValue.js file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -2,3 +2,35 @@
 const assertEqual = function(actual, expected) {
   return (actual === expected) ? console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const findKeyByValue = function(obj, val) {
+  let objKeys = Object.keys(obj);
+  let count = 0;
+  for (let element in obj) {
+    if (obj[element] === val) {
+      return objKeys[count];
+    }
+    count++;
+  }
+  return undefined;
+};
+
+
+//test cases
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+
+///a little too simple
+// const findKeyByValue = function(obj, val) {
+//   if (val in obj) {
+//     return obj[val];
+//   }
+//   return undefined
+// }
